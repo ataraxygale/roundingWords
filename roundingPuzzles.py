8881828a38788ef3a55668ef35770932e206189f
Angel <94824625+ataraxygale@users.noreply.github.com>
2025-03-26 14:58:43 -0600

    Add files via upload

---
+++ roundingPuzzles.py
@@ -0,0 +1,32 @@
+
+
+# your one-liner here to get just the first half (rounded down)
+
+import math
+
+word='amazing'
+#length halved with math.floor
+lenHalf=(len(word))/2
+roundedDown=math.floor(lenHalf)
+
+#list to create index of word
+wordIndex=list(word)
+
+#Prints first half of word as a list
+print("As a list: ", wordIndex[:roundedDown])
+
+#Prints first half of word as a string
+print("As a string: ", word[:roundedDown])
+
+#Prints second half of word as a list
+print("As a list: ", wordIndex[roundedDown:])
+
+#Prints second half of sowrd as a string
+print("As a string: ", word[roundedDown:])
+
+
+
+
+
+
+
